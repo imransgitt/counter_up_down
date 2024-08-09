812eedfeaf66c30314585713a01cb71dc00166d9
imranssgit <imransheikh32156@gmail.com>
2024-08-09 20:05:13 +0530

    second commit

--- counter.v
+++ counter.v
@@ -1,29 +1,11 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.08.2024 12:30:52
-// Design Name: 
-// Module Name: counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+//MOD N counter-->counting from 0 to N-1
+//K-->number of bits
 
-
-module counter #(parameter N = 3) (
+module counter #(parameter N = 7, K=3 ) (
         input i_clk,i_rst,i_en,i_up_down,
-        output reg [N-1:0]o_Q
+        output reg [K-1:0]o_Q
 );
 
 always@(posedge i_clk , posedge i_rst , posedge i_en)
@@ -37,7 +19,7 @@ begin
         begin
                 if(i_en)
                 begin
-                        o_Q <= (i_up_down) ? ((o_Q==6)?(0):(o_Q+1'b1)) : ((o_Q==0)?(6):(o_Q-1'b1));
+                        o_Q <= (i_up_down) ? ((o_Q==N-1)?(0):(o_Q+1'b1)) : ((o_Q==0)?(N-1):(o_Q-1'b1));
                 end
                 else
                 begin
--- counter_tb.v
+++ counter_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.08.2024 12:33:15
-// Design Name: 
-// Module Name: counter_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module counter_tb #(parameter N=3) ();
 
